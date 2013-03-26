71112308b5b18d19c63d41f727662e0a11ab3a31
goldieswx <goldieswx@gmail.com>
2013-03-26 17:36:39 +0100

    Create paged.js

---
+++ paged.js
@@ -0,0 +1,15 @@
+'use strict';
+
+angular.module('paged')
+  .filter('pages',function(){
+    return function(a,pager) {
+      if (typeof a ==='undefined') return []; 
+      if (typeof pager === 'undefined') return  [];
+     
+      if (typeof pager.currentPage === 'undefined') pager.currentPage = pager.currentPage | 1;
+      if (typeof pager.pageSize === 'undefined') pager.pageSize = pager.pageSize | 8;
+      pager.numPages = Math.max(1,Math.ceil(a.length / pager.pageSize));
+
+      return a.slice((pager.currentPage-1)*(pager.pageSize),(pager.currentPage)*(pager.pageSize));
+    };
+ });
