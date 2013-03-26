ab65ac2cd9d40a75a9637f4b6bb9c13b6e1818f1
goldieswx <goldieswx@gmail.com>
2013-03-26 17:42:23 +0100

    Update paged.js

--- paged.js
+++ paged.js
@@ -6,8 +6,8 @@ angular.module('paged',['ui.bootstrap'])
       if (typeof a ==='undefined') return []; 
       if (typeof pager === 'undefined') return  [];
      
-      if (typeof pager.currentPage === 'undefined') pager.currentPage = pager.currentPage | 1;
-      if (typeof pager.pageSize === 'undefined') pager.pageSize = pager.pageSize | 8;
+      if (typeof pager.currentPage === 'undefined') pager.currentPage = 1;
+      if (typeof pager.pageSize === 'undefined') pager.pageSize = 8;
       pager.numPages = Math.max(1,Math.ceil(a.length / pager.pageSize));
 
       return a.slice((pager.currentPage-1)*(pager.pageSize),(pager.currentPage)*(pager.pageSize));
